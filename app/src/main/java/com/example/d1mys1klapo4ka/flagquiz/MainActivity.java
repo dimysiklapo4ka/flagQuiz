47a2b2e6857d4486c39b792f79f773b199bde602
dimysiklapo4ka <dimysiklapo4ka@gmail.com>
2017-05-23 05:02:51 +0300

    third commit, add comments and finish logic

--- app/src/main/java/com/example/d1mys1klapo4ka/flagquiz/MainActivity.java
+++ app/src/main/java/com/example/d1mys1klapo4ka/flagquiz/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.d1mys1klapo4ka.flagquiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +13,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    //ключи для чтения данных из sharedPreferences
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true;//Включение портретного режима
+    private boolean preferencesChanged = true;//Для уведомления об изменении настроек.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +32,117 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Задание значений по умолчанию в файле sharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        //Регистрация слушателя для изменений sharedPreferences
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(prefereferencesChangeListener);
+
+        //Определение размеров экрана
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        //для планшета значение phoneDevice присваивается false
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE){
+
+            phoneDevice = false;//т.е не соответствует размерам экрана
+        }
+
+        //Установка для телефонов только портретной ориентации
+        if (phoneDevice){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (preferencesChanged){
+            /**После задания настроек по умолчанию инициализировать MainActivityFragment
+             *и запустить викторину */
+            MainActivityFragment quizFragment = (MainActivityFragment)getSupportFragmentManager()
+                    .findFragmentById(R.id.quizFragment);//находим content_main
+
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+
+            quizFragment.resetQuiz();
+            preferencesChanged = false;
+        }
+    }
+
+    //Иконка меню должна отображаться на устройстве с портретной ориентацией
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //Получаем текущую ориентацию экрана
+        int orientation = getResources().getConfiguration().orientation;
+
+        //заполнение меню
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+
+            return true;
+        }else {
+            return false;
+        }
     }
 
+    //отображение SettingsActivity при запуске устройства в портретной ориентации
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
 
         return super.onOptionsItemSelected(item);
     }
+
+    //слушатель изменения конфигурации SharedPreferences приложения
+    private SharedPreferences.OnSharedPreferenceChangeListener prefereferencesChangeListener = new
+            SharedPreferences.OnSharedPreferenceChangeListener() {
+
+                //метод вызывается при внесении изменений в настройки
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+                preferencesChanged = true;//Свидетельствует что настройки были изменены
+
+                    MainActivityFragment quizFragment = (MainActivityFragment)
+                            getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+                    if (key.equals(CHOICES)){//если изменилось количество вариантов ответа
+
+                        quizFragment.updateGuessRow(sharedPreferences);
+                        quizFragment.resetQuiz();
+
+                    }else if(key.equals(REGIONS)) {//Если изменился регион
+
+                        Set<String> regions = sharedPreferences.getStringSet(REGIONS, null);
+
+                        if (regions != null && regions.size() > 0) {
+
+                            quizFragment.updateRegions(sharedPreferences);
+                            quizFragment.resetQuiz();
+
+                        } else {
+                            //должен быть хоть один регион, по умолчанию Северная Америка
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                            regions.add(getString(R.string.default_region));
+                            editor.putStringSet(REGIONS, regions);
+                            editor.apply();
+
+                            Toast.makeText(MainActivity.this, R.string.default_region, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                }
+            };
+
+
 }
--- app/src/main/java/com/example/d1mys1klapo4ka/flagquiz/MainActivityFragment.java
+++ app/src/main/java/com/example/d1mys1klapo4ka/flagquiz/MainActivityFragment.java
@@ -1,22 +1,223 @@
 package com.example.d1mys1klapo4ka.flagquiz;
 
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
-    }
+    //Строка используется при регистрации извещений об ошибках
+    private static final String TAG = "FlagQuiz Activity";
+
+    private static final int FLAG_IN_QUIZ = 10;//При каждой игре нужно отгадать заданное кол-во флагов
+
+    private List<String> fileNameList;//Имена файлов с флагами
+    private List<String> quizCountriesList;//Страны текущей викторины
+    private Set<String> regionsSet;//Регионы текущей викторины
+    private String correctAnswer;//Правильная страна для текущего флага
+    private int totalGuesses;//Кол-во попыток
+    private int correctAnswers;//Кол-во правильных ответов
+    private int guessRow;//Кол-во строк с кнопками вариантов
+    private SecureRandom random;//Генератор случайных чисел
+    private Handler handler;//Для задержки загрузки следующего флага
+    private Animation shakeAnimation;//Анимация неправильного ответа
+
+    private LinearLayout llQuiz;//Макет с викториной
+    private TextView tvQuestionsNumber;//Номер текущего вопроса
+    private ImageView ivFlag;//Для вывода флага
+    private LinearLayout[] quessLinearLayouts;//Строки с кнопками
+    private TextView tvAnswer;//Для правильного ответа
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        fileNameList = new ArrayList<>();
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
+
+        //Загрузка анимации для неправильных ответов
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(),R.anim.incorrect_shake);
+        shakeAnimation.setRepeatCount(3);
+
+        //Получение ссылок на компоненты графического интерфейса
+        llQuiz = (LinearLayout)view.findViewById(R.id.ll_quiz);
+        tvQuestionsNumber = (TextView)view.findViewById(R.id.tv_questionNumber);
+        ivFlag = (ImageView)view.findViewById(R.id.iv_flag);
+        quessLinearLayouts = new LinearLayout[4];
+        quessLinearLayouts[0] = (LinearLayout)view.findViewById(R.id.ll_row1);
+        quessLinearLayouts[1] = (LinearLayout)view.findViewById(R.id.ll_row2);
+        quessLinearLayouts[2] = (LinearLayout)view.findViewById(R.id.ll_row3);
+        quessLinearLayouts[3] = (LinearLayout)view.findViewById(R.id.ll_row4);
+
+        tvAnswer = (TextView)view.findViewById(R.id.tv_answer);
+
+        //Настройка слушателей для кнопок ответов
+        for (LinearLayout row : quessLinearLayouts){
+
+            for (int colum = 0; colum < row.getChildCount(); colum++){
+                Button button = (Button)row.getChildAt(colum);
+                button.setOnClickListener(guessButtonListener);
+            }
+
+        }
+
+        //Назначение текста для номера вопроса
+        tvQuestionsNumber.setText(getString(R.string.question, 1, FLAG_IN_QUIZ));
+        return view;
+    }
+
+    //Обновление количества строк с кнопками на основании обьекта SharedPreferences
+    public void updateGuessRows(SharedPreferences sharedPreferences){
+
+        //получение количества отображаемых вариантов ответа
+        String choices = sharedPreferences.getString(MainActivity.CHOICES, null);
+        guessRow = Integer.parseInt(choices) / 2;
+
+        //все компоненты LinearLayout скрываются
+        for (LinearLayout layout : quessLinearLayouts){
+            layout.setVisibility(View.GONE);
+        }
+
+        //отображаются нужные компоненты linearLayout
+        for (int row = 0; row <guessRow; row++){
+            quessLinearLayouts[row].setVisibility(View.VISIBLE);
+        }
+    }
+
+    //Обновление выбранных регионов по данным из SharedPreferences
+    public void updateRegions(SharedPreferences sharedPreferences){
+        regionsSet = sharedPreferences.getStringSet(MainActivity.REGIONS, null);
+    }
+
+    //Настройка и запуск следующей серии вопросов
+    public void resetQuiz(){
+        //
+        AssetManager asset = getActivity().getAssets();
+        fileNameList.clear();//
+
+        try {
+            //перебор всех регионов
+            for (String region : regionsSet){
+                String[] paths = asset.list(region);
+
+                for (String path : paths){
+                    fileNameList.add(path.replace(".png", ""));
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading image file names", e);
+        }
+
+        correctAnswers = 0;//сброс количества правильных ответов
+        totalGuesses = 0;//сброс общего количества попыток
+        quizCountriesList.clear();//
+
+        int flagCountries = 1;
+        int numberOfFlag = fileNameList.size();
+
+        //добавление FLAG_IN_QUIZ случайных файлов в quizCountriesList
+        while (flagCountries <= FLAG_IN_QUIZ){
+            int randomIndex = random.nextInt(numberOfFlag);
+
+            //получение случайного имени файла
+            String filename = fileNameList.get(randomIndex);
+
+            //Если регион включен но еще не был выбран
+            if (!quizCountriesList.contains(filename)){
+                quizCountriesList.add(filename);//
+                ++flagCountries;
+            }
+        }
+
+        loadNextFlag();
+    }
+
+    //
+    public void loadNextFlag(){
+        //
+        String nextImage = quizCountriesList.remove(0);
+        correctAnswer = nextImage;//
+        tvAnswer.setText("");//
+
+        //
+        tvQuestionsNumber.setText(getString(
+                R.string.question, (correctAnswers + 1), FLAG_IN_QUIZ));
+
+        //
+        String region = nextImage.substring(0, nextImage.indexOf('-'));
+
+        //
+        AssetManager assetManager = getActivity().getAssets();
+
+        //
+        //
+        try (InputStream stream = assetManager.open(region + "/" + nextImage + ".png")){
+            //
+            Drawable flag = Drawable.createFromStream(stream, nextImage);
+            ivFlag.setImageDrawable(flag);
+
+            animate(false);
+
+        } catch (IOException e) {
+
+            Log.e(TAG, "Error loading" + nextImage, e);
+        }
+
+        Collections.shuffle(fileNameList);//
+
+        //
+        int correct = fileNameList.indexOf(correctAnswer);
+        fileNameList.add(fileNameList.remove(correct));
+
+        //
+        for (int row = 0; row < guessRow; row++){
+            //
+            for (int column = 0; column < quessLinearLayouts[row].getChildCount(); column++){
+                //
+                Button newGuessButton = (Button)quessLinearLayouts[row].getChildAt(column);
+                newGuessButton.setEnabled(true);
+
+                //
+                String filename = fileNameList.get((row * 2) + column);
+                newGuessButton.setText(getCountryName(filename));
+            }
+        }
+
+        //
+        int row = random.nextInt(guessRow);//
+        int column = random.nextInt(2);//
+        LinearLayout randomRow = quessLinearLayouts[row];//
+        String countryName = getCountryName(correctAnswer);
+        ((Button) randomRow.getChildAt(column)).setText(countryName);
     }
 }
